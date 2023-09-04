da58d32f91d3664ecf04e127bc3f9f11dbc48386
cyjiin <90894850+cyjiin@users.noreply.github.com>
2023-09-05 07:58:36 +0900

    Update 0902.cpp

--- BOJ/BOJ/0902.cpp
+++ BOJ/BOJ/0902.cpp
@@ -7,45 +7,48 @@ int main() {
 	int* arrA = new int[N]; // 입력받은 길이를 동적으로 배열 할당
 	int* arrB = new int[N];
 	int* arrP = new int[N];
-	int* arrBP = new int[N];
 
 
 	for (int i = 0; i < N; i++) {
 		cin >> arrA[i];
 	} // 배열의 원소를 입력받음
 
-	/*for (int i = 0; i < N; i++) {
-		arrP[i] = i;
-	}*/
+	
 
 	for (int i = 0; i < N; i++) {
-		arrBP[i] = arrA[i];
+		arrB[i] = arrA[i];
 	} // B[P[i]]=A[i] 적용
 
 
 
-	int temp = arrBP[0]; // 교환시 필요한 임시 저장변수 선언 
+	int temp = arrB[0]; // 교환시 필요한 임시 저장변수 선언 
 						// 배열의 첫번째 원소로 초기화
 
 	for (int i = 0; i < N; i++) {	// BP배열을 비교 정렬 반복
 		for (int j = 0; j < N - 1; j++) {
-			if (arrBP[j] > arrBP[j + 1]) {
-				temp = arrBP[j];
-				arrBP[j] = arrBP[j + 1];
-				arrBP[j + 1] = temp;	// 비내림차순을 만들기위해서 앞 숫자가 더 클 경우 바로 뒤 원소와 자리를 교환
+			if (arrB[j] > arrB[j + 1]) {
+				temp = arrB[j];
+				arrB[j] = arrB[j + 1];
+				arrB[j + 1] = temp;	// 비내림차순을 만들기위해서 앞 숫자가 더 클 경우 바로 뒤 원소와 자리를 교환
 			}
 		}
 	}
 
+
 	for (int i = 0; i < N; i++) {
-		arrB[i] = arrBP[i];
-	} // BP의 원소 B에 할당
+		for (int j = 0; j < N; j++) {
+			if (arrA[i] == arrB[j]) {
+					arrP[i] = j;
+					break;
+			}
+		}
+	}
 
 
 
 	for (int i = 0; i < N; i++) { // 배열 원소 출력
-		cout << arrB[i] << endl;
+		cout << arrP[i] << ' ';
 	}
 
 	return 0;
-} // 1015번 다시하기
+} // 1015번
